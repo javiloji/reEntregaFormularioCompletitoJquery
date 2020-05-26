a27a1be58b13cc15b38b3278453978606bdcd522
javier <javierlopera988@gmail.com>
2020-05-27 01:07:51 +0200

    Primera subida

---
+++ js/main.js
@@ -0,0 +1,86 @@
+/**
+     * Clase de inicio
+     * 
+     * @author Javier Lopera Jiménez
+     * 
+    */
+
+$(function () {
+
+    function rellenaSpam($control) {
+
+        // comprobar el checkbox
+        if ($control[0] == $(":checkbox")[0]) {
+            // alert("a");
+            let mensaje = validar.comprobarCheckbox($control);
+            $control.next().text(mensaje);
+            // alert(mensaje);
+        }
+        else if ($control.prop("type") === "radio") {
+            let mensaje = validar.comprobarRadio(document.getElementsByName("radio"));
+            $control.parent().parent().children("span").text(mensaje);
+        }
+        else {
+
+            let mensaje = validar[$control.attr("id")]($control.val());
+            $control.next().text(mensaje);
+            
+            return (mensaje.length != 0);
+        }
+    }
+
+    $("input,select")
+        .blur(function () {
+            if ($(this).prop("type") === "select-one") {
+                let mensaje = validar.comprobarSelect($(this).val());
+                $(this).parent().children("span").text(mensaje);
+            }
+            else {
+                if (!rellenaSpam($(this))) {
+                    $(this).removeClass("error");
+                    $(this).addClass("valido");
+                }
+                else
+                    $(this).addClass("error");
+            }
+        })
+        .focus(function () {
+            $(this).removeClass("valido error");
+        });
+
+    $("form")
+        .submit(function (event) {
+
+            event.preventDefault();
+
+            $("input, select").trigger("blur");
+
+            if ($("span:not(:empty)").length == 0) {
+                $("form").fadeOut("slow").fadeIn("slow");
+                setTimeout(function () {
+                    $("form").trigger("reset");
+                }, 400);
+            } else {
+                $("span:not(:empty)").first().prev().focus();
+            }
+        })
+        .on("reset", function () {
+            $("input,select").removeClass("valido").removeClass("error");
+            $("span").text("");
+        });
+
+    $("#rellenar")
+        .click(function (event) {
+
+            $("#comprobarTexto").val("Hola");
+            $("#comprobarNumero").val("98");
+            $("#comprobarCorreo").val("javiLoji@gmail.com");
+            $("#comprobarDni").val("12345678Z");
+            $("#comprobarFechaNacimiento").val("29/02/2000");
+            $("#comprobarTelefono").val("658542122");
+            $("#comprobarUrl").val("https://amazon.com");
+
+            event.preventDefault();
+
+        })
+})
---
+++ js/validar.js
@@ -0,0 +1,138 @@
+
+
+/**
+ * Clase que valida todas las expresiones regulares
+ * 
+ * @author Javier Lopera Jiménez
+ * 
+*/
+
+// Arrays donde se almacenarán las expresiones regulares y el error correspondiente 
+// en caso de no ser válido
+
+
+validar = (function () {
+
+    let expresiones = {
+        arrayTexto: [new RegExp("^[a-zA-Zá-úÁ-Ú0-9 ]+$"), "Se debe escribir un texto"],
+        arrayNumero: [new RegExp("^[0-9]+$"), "Se debe escribir un número entero"],
+        arrayFecha: [new RegExp("^([0-9]{2})([/-])([0-9]{2})\\2([0-9]{4})$"), "Introduce un formato válido, '22/12/2222' o '29-02-2000'"],
+        arrayDni: [new RegExp("^([0-9]{8})[- ]?([a-zA-Z])$"), "12345678Z o 12345678-Z", "TRWAGMYFPDXBNJZSQVHLCKET"],
+        arrayCorreo: [new RegExp("^([0-9a-zA-Z]+[.]?)*[@][0-9a-zA-Z]+[.][a-zA-Z]+$"), "Se debe poner el correo correctamente"],
+        arrayTelefono: [new RegExp("^\\(?[+]?[0-9]?[0-9]?[0-9]?[0-9]?\\)?[ ]?[0-9]{3}[ ]?[0-9]{3}[ ]?[0-9]{3}[ ]?$"), "Introduce un número válido"],
+        arrayUrl: [new RegExp("^(http[s]?[:][/][/])?(www[.])?[a-zA-Z]+([.][a-zA-Z]+([/][a-zA-Z0-9]+)*)*$"), "Se debe poner la url correctamente"],
+    }
+
+    // Comprueba que el campo sea correcto
+
+    let comprobarTexto = function (texto){
+        if(!expresiones.arrayTexto[0].test(texto))
+            return expresiones.arrayTexto[1];
+        return "";
+    }
+
+    let comprobarNumero = function (numero){
+        if(!expresiones.arrayNumero[0].test(numero))
+            return expresiones.arrayNumero[1];
+        return "";
+    }
+
+    let comprobarCorreo = function (correo){
+        if(!expresiones.arrayCorreo[0].test(correo))
+            return expresiones.arrayCorreo[1];
+        return "";
+    }
+
+    let comprobarTelefono = function (telefono){
+        if(!expresiones.arrayTelefono[0].test(telefono))
+            return expresiones.arrayTelefono[1];
+        return "";
+    }
+
+    let comprobarUrl = function (url){
+        if(!expresiones.arrayUrl[0].test(url))
+            return expresiones.arrayUrl[1];
+        return "";
+    }
+
+    // Comprueba si el checkBox esta activo
+
+    let comprobarCheckbox = function (input) {
+        if (!input.prop("checked")) {
+            return "Este campo debe estar seleccionado";
+        }
+        return "";
+    }
+
+    // Comprueba que el radio Button no quede vacio
+
+    let comprobarRadio = function (opciones) {
+
+        if(!Array.from(opciones).some(elemento => (elemento.checked))){
+            return "Debes seleccionar alguna opción";
+        }
+        return "";
+    }
+
+    // Comprueba que el radio Button no quede vacio
+
+    let comprobarSelect = function (opcion) {
+        if (opcion == "") {
+            return "Debes seleccionar algún campo";
+        }
+        return "";
+    }
+
+    function comprobarFechaNacimiento(fecha) {
+
+        try {
+
+            let [, dia, , mes, anno] = expresiones.arrayFecha[0].exec(fecha.trim());
+
+            let date = new Date(`${anno}/${mes}/${dia}`);
+
+            if (Number(dia) != date.getDate() || Number(mes) != date.getMonth() + 1 || Number(anno) != date.getFullYear()) {
+                return "La fecha no es válida";
+            }
+
+            return "";
+
+        } catch (error) {
+            if (fecha.trim() == "") {
+                return "El campo no puede quedar vacio";
+            }
+            return expresiones.arrayFecha[1];
+        }
+    }
+
+    // Comprueba que el Dni sea correcto
+
+    let comprobarDni = function (dniCompleto) {
+
+        try {
+            let [, dniNumero, dniLetra] = expresiones.arrayDni[0].exec(dniCompleto.trim());
+
+            if (dniLetra.toUpperCase() == expresiones.arrayDni[2][parseInt(dniNumero) % 23]) {
+                return "";
+            }
+            return "La letra no corresponde con el dni";
+
+        } catch{
+            return expresiones.arrayDni[1];
+        }
+    }
+
+    return {
+        comprobarTexto: comprobarTexto,
+        comprobarNumero: comprobarNumero,
+        comprobarCorreo: comprobarCorreo,
+        comprobarTelefono: comprobarTelefono,
+        comprobarUrl: comprobarUrl,
+        comprobarCheckbox: comprobarCheckbox,
+        comprobarRadio: comprobarRadio,
+        comprobarSelect: comprobarSelect,
+        comprobarDni: comprobarDni,
+        comprobarFechaNacimiento: comprobarFechaNacimiento,
+        expresiones: expresiones
+    }
+})();
